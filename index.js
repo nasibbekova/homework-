4b2239270affacff2a21532107941785e481f91b
Madina <alimon=vam.6703@gmail.com>
2022-12-13 13:26:20 +0500

    first commit

---
+++ index.js
@@ -0,0 +1,58 @@
+// 1
+let cars = [
+    {
+    mark:'Mercedes',
+    price: 5646,
+    },
+    {
+    mark:'BMW',
+    price:35000,
+    },
+    {
+    mark:'Audi',
+    price:12581,
+    },
+    {
+    mark:'KIA',
+    price:64567,
+    },
+    {
+    markFive: 'Hyundai',
+    price:35000
+    }
+]
+
+let cheap = []
+let other = []
+
+cars.filter (car =>{
+    if (car.price  === 35000) {
+    cheap ++
+} else {
+    other ++
+}
+})
+
+console.log (
+   cheap,
+   other
+)
+
+// 2
+let index = prompt()
+
+let names = ['Aleksey', 'Kartoshka', 'Margarita' , 'Morgenshtern', 'Monica']
+
+let longestName =names.reduce((a,b)=> a.length > b.length ? a:b)
+
+console.log (longestName)
+
+// 3
+let total = 0
+
+let arr = [[[{a:{price: 20}}] [{a:{price: 35}}] [{a:{price: 44}}]]]
+
+if (arr[1])arr[1].price += arr.shift().price.push(total)
+
+    console.log(total);
+
